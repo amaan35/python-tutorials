5ef93e0d916fd9c7625f0ac1d4f7e1d31d1138f9
amaan35 <amaanlm35@gmail.com>
2023-09-30 20:56:45 +0530

    Strings tutorials

---
+++ StringMethods/main.py
@@ -0,0 +1,10 @@
+# concatenating two strings using +
+print("hello" + "world")
+# printing string multiple times using * operator
+print("a"*2)
+# the below code does not print any string
+print("a"*-2)
+# typecasting string into integer
+print(int("234"))
+# typecasting integer into string
+print(str(234))

75ae70a4f0eab6a36c864b39532e2e8d84ee60df
amaan35 <amaanlm35@gmail.com>
2023-09-30 11:52:37 +0530

    program to understand the use of operators

---
+++ operators/main.py
@@ -0,0 +1,21 @@
+# a program to practice operators
+# unary operators
+print(-2)
+print(+1)
+# exponential operator
+print(2**3)
+# multiplication operator
+print(2*3)
+# division operator - gives us the answer in float even if both operands are int
+print(4/2)
+print(4/3)
+# floor division operator - gives us the answer in int only if both the operands are int and
+# prints the closest lesser int value if the answer is floated
+print(4//2)
+print(4//3)
+# modulo operator - gives us the remainder
+print(4 % 2)
+print(4 % 3)
+# binary operator
+print(1+2)
+print(1-2)

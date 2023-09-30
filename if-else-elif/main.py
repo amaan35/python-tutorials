5ed3ea152c1108118941246fd7c1435d03653da7
amaan35 <amaanlm35@gmail.com>
2023-09-30 21:13:28 +0530

    if-else-elif statements tutorial

---
+++ if-else-elif/main.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age "))
+if age > 18:
+    print("You can vote")
+elif age == 18:
+    print("you are exactly 18 years old and vote")
+else:
+    print("You cannot vote")

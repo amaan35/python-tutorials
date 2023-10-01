25adbbefa9d24154d1caf3fcadca05c999d5ff07
amaan35 <amaanlm35@gmail.com>
2023-10-01 09:26:35 +0530

    bitwise operators tutorial

---
+++ BitwiseOperators/main.py
@@ -0,0 +1,12 @@
+# & - conjunction operator - returns 1 only if both the binaries are 1
+print(15 & 22)
+# | - disjunction operator - returns 1 if at least one binary is 1
+print(15 | 22)
+# ^ - exclusive OR operator - returns 1 only if exactly one binary is 1
+print(15 ^ 22)
+# ~ - negation operator - returns opposite of the bit
+print(~22)
+# >> - bit right shift operator - shifts all the bits in binary to the right
+print(22 >> 1)
+# << - bit left shift operator - shifts all the bits in binary to the left
+print(22 << 2)

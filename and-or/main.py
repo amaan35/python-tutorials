ac1c00a548a85a107bb2d495fefc3d55ef33e0c4
amaan35 <amaanlm35@gmail.com>
2023-10-01 09:10:16 +0530

    and-or operators tutorial

---
+++ and-or/main.py
@@ -0,0 +1,7 @@
+# combining two or more conditions
+num = 2
+if num > 5 and num < 15:
+    print("num is greater than 5 and less than 15")
+elif num > 15 or num < 20:
+    print("num is greater than 15 or less than 20")
+

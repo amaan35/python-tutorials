bf9d7b9967bac56373018058cb35dd669f784202
amaan35 <amaanlm35@gmail.com>
2023-10-02 16:06:07 +0530

    arguments in functions tutorial

---
+++ functions-arguments/main.py
@@ -0,0 +1,8 @@
+def input_num(num1, num2=2):
+    number1 = int(input("Enter number1 : ")) * num1
+    number2 = int(input("Enter number2 : ")) * num2
+    print(number1, number2)
+
+
+input_num(10)
+input_num(10, 4)
